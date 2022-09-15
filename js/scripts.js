07cf47d9f1933bcf075e0c1d877717ecf79f6f67
pablo <pavel.petukhov.003@gmail.com>
2022-09-15 21:11:44 +0300

    qqq

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,52 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        center:true,
-        nav:true,
-        autoplay:true,
-        autoplayTimeout:4000,
+        loop: true,
+        margin: 0,
+        center: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
         autoplaySpeed: 2000,
-        navText: [ '', ' ' ],
+        navText: ['', ' '],
 
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
-            800:{
-                items:2
+            800: {
+                items: 2
             },
 
-            1000:{
-                items:3
+            1000: {
+                items: 3
             }
         }
+
+    });
+});
+
+$(document).ready(function () {
+    $('.header_burger').click(function (event) {
+        $('.header_burger,.header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+
+$(document).ready(function () {
+    var btn = $('#button');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, '300');
     });
 });
